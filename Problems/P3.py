aa8ac6ee55eabfbc0e39b151b30edd07fc502df2
Veronika <makovac.veronika@gmail.com>
2024-05-23 21:25:56 +0200

    Popravljena verzija

--- Problems/P3.py
+++ Problems/P3.py
@@ -1,24 +1,22 @@
 600851475143
-n = int(input("Input your number:"))
+main_number = int(input("Input your number:"))
 
 
-def trial_division(n: int) -> list[int]:
+def trial_division(main_number: int) -> list[int]:
     """Return a list of the prime factors for a natural number."""
-    a = []               # Prepare an empty list.
-    f = 2                # The first possible factor.    
-    while n > 1:         # While n still has remaining factors...
-        if n % f == 0:   # The remainder of n divided by f might be zero.        
-            a.append(f)  # If so, it divides n. Add f to the list.
-            n //= f      # Divide that factor out of n.
+    prime_factors = []               # Prepare an empty list.
+    factor = 2                # The first possible factor.    
+    while main_number > 1:         # While n still has remaining factors...
+        if main_number % factor == 0:   # The remainder of n divided by f might be zero.        
+            prime_factors.append(factor)  # If so, it divides n. Add f to the list.
+            main_number //= factor      # Divide that factor out of n.
         else:            # But if f is not a factor of n,
-            f += 1       # Add one to f and try again.
-    return a             # Prime factors may be repeated: 12 factors to 2,2,3.
+            factor += 1       # Add one to f and try again.
+    return prime_factors             # Prime factors may be repeated: 12 factors to 2,2,3.
 
 largest = 0
-for number in trial_division(n):
+for number in trial_division(main_number):
     if number > largest:
         largest = number
     
 print(number)
-
-
